7cdb332f6075a898a2b184ff661837542c11e4c9
Dmytro Khabaznia <Dmytro_Khabaznia@epam.com>
2021-02-14 11:46:34 +0200

    Init commit

---
+++ src/main/groovy/com/epam/cloud_training/CloudTrainingApplication.groovy
@@ -0,0 +1,14 @@
+package com.epam.cloud_training
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ComponentScan
+
+@SpringBootApplication
+class CloudTrainingApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(CloudTrainingApplication, args)
+	}
+
+}
